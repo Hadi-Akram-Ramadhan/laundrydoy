4afd1f2b4f5ce3445f1d6b8b23c720f64fedbe84
Hadi-Akram-Ramadhan <kk.hadi.akram@gmail.com>
2025-02-12 11:34:48 +0700

    Fix typo and improve code formatting in multiple files
    - Corrected "Pemabayaran" to "Pembayaran" in transaction tables
    - Removed copyright text from footer
    - Cleaned up code formatting and indentation in layout_footer.php, transaksi.php, index.php, and laporan.php
    - Simplified HTML structure and removed unnecessary whitespace

--- kasir/transaksi.php
+++ kasir/transaksi.php
@@ -5,11 +5,12 @@ require 'layout_header.php';
 $query = "SELECT transaksi.*,member.nama_member , detail_transaksi.total_harga FROM transaksi INNER JOIN member ON member.id_member = transaksi.member_id INNER JOIN detail_transaksi ON detail_transaksi.transaksi_id = transaksi.id_transaksi WHERE transaksi.outlet_id = " . $_SESSION['outlet_id'];
 $data = ambildata($conn,$query);
 
-?> 
+?>
 <div class="container-fluid">
     <div class="row bg-title">
         <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
-            <h4 class="page-title">Data Master <?= $title ?></h4> </div>
+            <h4 class="page-title">Data Master <?= $title ?></h4>
+        </div>
         <div class="col-lg-9 col-sm-8 col-md-8 col-xs-12">
             <ol class="breadcrumb">
                 <li><a href="#">Paket</a></li>
@@ -22,11 +23,14 @@ $data = ambildata($conn,$query);
             <div class="white-box">
                 <div class="row">
                     <div class="col-md-6">
-                        <a href="transaksi_cari_member.php" class="btn btn-primary box-title"><i class="fa fa-plus fa-fw"></i> Tambah</a>
-                         <a href="transaksi_konfirmasi.php" class="btn btn-primary box-title"><i class="fa fa-check fa-fw"></i> Konfirmasi Pembayaran</a>
+                        <a href="transaksi_cari_member.php" class="btn btn-primary box-title"><i
+                                class="fa fa-plus fa-fw"></i> Tambah</a>
+                        <a href="transaksi_konfirmasi.php" class="btn btn-primary box-title"><i
+                                class="fa fa-check fa-fw"></i> Konfirmasi Pembayaran</a>
                     </div>
                     <div class="col-md-6 text-right">
-                        <button id="btn-refresh" class="btn btn-primary box-title text-right" title="Refresh Data"><i class="fa fa-refresh" id="ic-refresh"></i></button>
+                        <button id="btn-refresh" class="btn btn-primary box-title text-right" title="Refresh Data"><i
+                                class="fa fa-refresh" id="ic-refresh"></i></button>
                     </div>
                 </div>
                 <div class="table-responsive">
@@ -37,7 +41,7 @@ $data = ambildata($conn,$query);
                                 <th>Invoice</th>
                                 <th>Member</th>
                                 <th>Status</th>
-                                <th>Pemabayaran</th>
+                                <th>Pembayaran</th>
                                 <th>Total Harga</th>
                                 <th width="15%">Aksi</th>
                             </tr>
@@ -45,19 +49,21 @@ $data = ambildata($conn,$query);
                         <tbody>
                             <?php if(isset($data) != null): ?>
                             <?php foreach($data as $transaksi): ?>
-                                <tr>
-                                    <td></td>
-                                    <td><?= $transaksi['kode_invoice'] ?></td>
-                                    <td><?= $transaksi['nama_member'] ?></td>
-                                    <td><?= $transaksi['status'] ?></td>
-                                    <td><?= $transaksi['status_bayar'] ?></td>
-                                    <td><?= $transaksi['total_harga'] ?></td>
-                                    <td align="center">
-                                          <a href="transaksi_detail.php?id=<?= $transaksi['id_transaksi']; ?>" data-toggle="tooltip" data-placement="bottom" title="Edit" class="btn btn-success btn-block">Detail</a>
-                                    </td>
-                                </tr>
+                            <tr>
+                                <td></td>
+                                <td><?= $transaksi['kode_invoice'] ?></td>
+                                <td><?= $transaksi['nama_member'] ?></td>
+                                <td><?= $transaksi['status'] ?></td>
+                                <td><?= $transaksi['status_bayar'] ?></td>
+                                <td><?= $transaksi['total_harga'] ?></td>
+                                <td align="center">
+                                    <a href="transaksi_detail.php?id=<?= $transaksi['id_transaksi']; ?>"
+                                        data-toggle="tooltip" data-placement="bottom" title="Edit"
+                                        class="btn btn-success btn-block">Detail</a>
+                                </td>
+                            </tr>
                             <?php endforeach; ?>
-                        <?php endif; ?>
+                            <?php endif; ?>
                         </tbody>
                     </table>
                 </div>
@@ -68,7 +74,7 @@ $data = ambildata($conn,$query);
     <!-- table -->
     <!-- ============================================================== -->
     <div class="row">
-        
+
     </div>
 </div>
 <?php
--- owner/index.php
+++ owner/index.php
@@ -7,11 +7,12 @@ $jPelanggan = ambilsatubaris($conn,'SELECT COUNT(id_member) as jumlahmember FROM
 $joutlet = ambilsatubaris($conn,'SELECT COUNT(id_outlet) as jumlahoutlet FROM outlet');
 $query = "SELECT transaksi.*,member.nama_member , detail_transaksi.total_harga FROM transaksi INNER JOIN member ON member.id_member = transaksi.member_id INNER JOIN detail_transaksi ON detail_transaksi.transaksi_id = transaksi.id_transaksi   ORDER BY transaksi.id_transaksi DESC LIMIT 10";
 $data = ambildata($conn,$query);
-?> 
+?>
 <div class="container-fluid">
     <div class="row bg-title">
         <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
-            <h4 class="page-title">Dashboard</h4> </div>
+            <h4 class="page-title">Dashboard</h4>
+        </div>
         <div class="col-lg-9 col-sm-8 col-md-8 col-xs-12">
             <ol class="breadcrumb">
                 <li><a href="#">Dashboard</a></li>
@@ -32,7 +33,8 @@ $data = ambildata($conn,$query);
                     <li>
                         <div id="sparklinedash"></div>
                     </li>
-                    <li class="text-right"><i class="ti-arrow-up text-success"></i> <span class="counter text-success"><?= $joutlet['jumlahoutlet'] ?></span></li>
+                    <li class="text-right"><i class="ti-arrow-up text-success"></i> <span
+                            class="counter text-success"><?= $joutlet['jumlahoutlet'] ?></span></li>
                 </ul>
             </div>
         </div>
@@ -43,7 +45,8 @@ $data = ambildata($conn,$query);
                     <li>
                         <div id="sparklinedash2"></div>
                     </li>
-                    <li class="text-right"><i class="ti-arrow-up text-purple"></i> <span class="counter text-purple"><?= $jPelanggan['jumlahmember'] ?></span></li>
+                    <li class="text-right"><i class="ti-arrow-up text-purple"></i> <span
+                            class="counter text-purple"><?= $jPelanggan['jumlahmember'] ?></span></li>
                 </ul>
             </div>
         </div>
@@ -54,7 +57,8 @@ $data = ambildata($conn,$query);
                     <li>
                         <div id="sparklinedash3"></div>
                     </li>
-                    <li class="text-right"><i class="ti-arrow-up text-info"></i> <span class="counter text-info"><?= $jTransaksi['jumlahtransaksi'] ?></span></li>
+                    <li class="text-right"><i class="ti-arrow-up text-info"></i> <span
+                            class="counter text-info"><?= $jTransaksi['jumlahtransaksi'] ?></span></li>
                 </ul>
             </div>
         </div>
@@ -71,24 +75,26 @@ $data = ambildata($conn,$query);
                                 <th>Invoice</th>
                                 <th>Member</th>
                                 <th>Status</th>
-                                <th>Pemabayaran</th>
+                                <th>Pembayaran</th>
                                 <th>Total Harga</th>
                                 <th width="15%">Aksi</th>
                             </tr>
                         </thead>
                         <tbody>
                             <?php $no=1; foreach($data as $transaksi): ?>
-                                <tr>
-                                    <td><?= $no++ ?></td>
-                                    <td><?= $transaksi['kode_invoice'] ?></td>
-                                    <td><?= $transaksi['nama_member'] ?></td>
-                                    <td><?= $transaksi['status'] ?></td>
-                                    <td><?= $transaksi['status_bayar'] ?></td>
-                                    <td><?= $transaksi['total_harga'] ?></td>
-                                    <td align="center">
-                                          <a href="transaksi_detail.php?id=<?= $transaksi['id_transaksi']; ?>" data-toggle="tooltip" data-placement="bottom" title="Edit" class="btn btn-success btn-block">Detail</a>
-                                    </td>
-                                </tr>
+                            <tr>
+                                <td><?= $no++ ?></td>
+                                <td><?= $transaksi['kode_invoice'] ?></td>
+                                <td><?= $transaksi['nama_member'] ?></td>
+                                <td><?= $transaksi['status'] ?></td>
+                                <td><?= $transaksi['status_bayar'] ?></td>
+                                <td><?= $transaksi['total_harga'] ?></td>
+                                <td align="center">
+                                    <a href="transaksi_detail.php?id=<?= $transaksi['id_transaksi']; ?>"
+                                        data-toggle="tooltip" data-placement="bottom" title="Edit"
+                                        class="btn btn-success btn-block">Detail</a>
+                                </td>
+                            </tr>
                             <?php endforeach; ?>
                         </tbody>
                     </table>
@@ -99,4 +105,4 @@ $data = ambildata($conn,$query);
 </div>
 <?php
 require 'layout_footer.php';
-?> 
+?>
