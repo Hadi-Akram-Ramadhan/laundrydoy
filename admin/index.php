33a3963329d0a30636491825f33bbcb7039045ac
Hadi-Akram-Ramadhan <kk.hadi.akram@gmail.com>
2025-02-20 11:32:22 +0700

    -

--- admin/index.php
+++ admin/index.php
@@ -1,4 +1,5 @@
 <?php
+
 $title = 'dashboard';
 require 'functions.php';
 require 'layout_header.php';
@@ -7,14 +8,15 @@ $jPelanggan = ambilsatubaris($conn,'SELECT COUNT(id_member) as jumlahmember FROM
 $joutlet = ambilsatubaris($conn,'SELECT COUNT(id_outlet) as jumlahoutlet FROM outlet');
 $query = "SELECT transaksi.*,member.nama_member , detail_transaksi.total_harga FROM transaksi INNER JOIN member ON member.id_member = transaksi.member_id INNER JOIN detail_transaksi ON detail_transaksi.transaksi_id = transaksi.id_transaksi   ORDER BY transaksi.id_transaksi DESC LIMIT 10";
 $data = ambildata($conn,$query);
-?> 
+?>
 <div class="container-fluid">
-    <div class="row bg-title">
-        <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
-            <h4 class="page-title">Dashboard</h4> </div>
-        <div class="col-lg-9 col-sm-8 col-md-8 col-xs-12">
+    <div class="doy row bg-title">
+        <div class="doy col-lg-3 col-md-4 col-sm-4 col-xs-12">
+            <h4 class="page-title-header">Dashboard</h4>
+        </div>
+        <div class="doy col-lg-9 col-sm-8 col-md-8 col-xs-12">
             <ol class="breadcrumb">
-                <li><a href="#">Dashboard</a></li>
+                <li><a href="#" class="page-title-header">Dashboard</a></li>
             </ol>
         </div>
         <!-- /.col-lg-12 -->
@@ -32,7 +34,8 @@ $data = ambildata($conn,$query);
                     <li>
                         <div id="sparklinedash"></div>
                     </li>
-                    <li class="text-right"><i class="ti-arrow-up text-success"></i> <span class="counter text-success"><?= htmlspecialchars($joutlet['jumlahoutlet']); ?></span></li>
+                    <li class="text-right"><i class="ti-arrow-up text-success"></i> <span
+                            class="counter text-success"><?= htmlspecialchars($joutlet['jumlahoutlet']); ?></span></li>
                 </ul>
             </div>
         </div>
@@ -43,7 +46,9 @@ $data = ambildata($conn,$query);
                     <li>
                         <div id="sparklinedash2"></div>
                     </li>
-                    <li class="text-right"><i class="ti-arrow-up text-purple"></i> <span class="counter text-purple"><?= htmlspecialchars($jPelanggan['jumlahmember']); ?></span></li>
+                    <li class="text-right"><i class="ti-arrow-up text-purple"></i> <span
+                            class="counter text-purple"><?= htmlspecialchars($jPelanggan['jumlahmember']); ?></span>
+                    </li>
                 </ul>
             </div>
         </div>
@@ -54,7 +59,9 @@ $data = ambildata($conn,$query);
                     <li>
                         <div id="sparklinedash3"></div>
                     </li>
-                    <li class="text-right"><i class="ti-arrow-up text-info"></i> <span class="counter text-info"><?= htmlspecialchars($jTransaksi['jumlahtransaksi']); ?></span></li>
+                    <li class="text-right"><i class="ti-arrow-up text-info"></i> <span
+                            class="counter text-info"><?= htmlspecialchars($jTransaksi['jumlahtransaksi']); ?></span>
+                    </li>
                 </ul>
             </div>
         </div>
@@ -78,17 +85,19 @@ $data = ambildata($conn,$query);
                         </thead>
                         <tbody>
                             <?php $no=1; foreach($data as $transaksi): ?>
-                                <tr>
-                                    <td><?= $no++ ?></td>
-                                    <td><?= htmlspecialchars($transaksi['kode_invoice']); ?></td>
-                                    <td><?= htmlspecialchars($transaksi['nama_member']); ?></td>
-                                    <td><?= htmlspecialchars($transaksi['status']); ?></td>
-                                    <td><?= htmlspecialchars($transaksi['status_bayar']); ?></td>
-                                    <td><?= htmlspecialchars($transaksi['total_harga']); ?></td>
-                                    <td align="center">
-                                          <a href="transaksi_detail.php?id=<?= htmlspecialchars($transaksi['id_transaksi']); ?>" data-toggle="tooltip" data-placement="bottom" title="Edit" class="btn btn-success btn-block">Detail</a>
-                                    </td>
-                                </tr>
+                            <tr>
+                                <td><?= $no++ ?></td>
+                                <td><?= htmlspecialchars($transaksi['kode_invoice']); ?></td>
+                                <td><?= htmlspecialchars($transaksi['nama_member']); ?></td>
+                                <td><?= htmlspecialchars($transaksi['status']); ?></td>
+                                <td><?= htmlspecialchars($transaksi['status_bayar']); ?></td>
+                                <td><?= htmlspecialchars($transaksi['total_harga']); ?></td>
+                                <td align="center">
+                                    <a href="transaksi_detail.php?id=<?= htmlspecialchars($transaksi['id_transaksi']); ?>"
+                                        data-toggle="tooltip" data-placement="bottom" title="Edit"
+                                        class="btn btn-success btn-block">Detail</a>
+                                </td>
+                            </tr>
                             <?php endforeach; ?>
                         </tbody>
                     </table>
@@ -99,4 +108,4 @@ $data = ambildata($conn,$query);
 </div>
 <?php
 require 'layout_footer.php';
-?> 
+?>
