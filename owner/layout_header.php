ecc24cf8fa218256156eb4bce10709b5073ca429
Hadi-Akram-Ramadhan <kk.hadi.akram@gmail.com>
2025-02-26 06:47:44 +0700

    Remove mobile navigation toggle from layout headers
    - Delete redundant mobile navigation toggle in admin, kasir, and owner layout headers
    - Simplify top navigation bar by removing unnecessary responsive menu button
    - Maintain consistent header layout across different user roles

--- owner/layout_header.php
+++ owner/layout_header.php
@@ -207,10 +207,7 @@
                 </div>
                 <!-- /Logo -->
                 <ul class="nav navbar-top-links navbar-right pull-right">
-                    <li>
-                        <a class="nav-toggler open-close waves-effect waves-light hidden-md hidden-lg"
-                            href="javascript:void(0)"><i class="fa fa-bars"></i></a>
-                    </li>
+
                     <li>
                         <a class="profile-pic" href="#"> <img src="../assets/plugins/images/users/varun.jpg"
                                 alt="user-img" width="36" class="img-circle"><b
