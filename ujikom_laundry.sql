4e063098ef4d27c47bb45ca46485d5e2a2e3d57d
Hadi-Akram-Ramadhan <kk.hadi.akram@gmail.com>
2025-02-26 08:40:34 +0700

    Update database schema and sample data
    - Upgrade phpMyAdmin and server version metadata
    - Update sample data for members, outlets, pakets, and transactions
    - Add img column to user table
    - Modify character set and collation settings
    - Increment auto-increment values for various tables

--- ujikom_laundry.sql
+++ ujikom_laundry.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.1
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: May 29, 2020 at 06:06 PM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.4.3
+-- Host: 127.0.0.1
+-- Generation Time: Feb 26, 2025 at 02:39 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -36,14 +35,15 @@ CREATE TABLE `detail_transaksi` (
   `total_harga` double DEFAULT NULL,
   `keterangan` text DEFAULT NULL,
   `total_bayar` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `detail_transaksi`
 --
 
 INSERT INTO `detail_transaksi` (`id_detail`, `transaksi_id`, `paket_id`, `qty`, `total_harga`, `keterangan`, `total_bayar`) VALUES
-(9, 33, 4, 10, 150000, NULL, 200000);
+(14, 38, 8, 10, 900000, NULL, 1000000),
+(15, 39, 6, 90, 1080000, NULL, 2147483647);
 
 -- --------------------------------------------------------
 
@@ -58,14 +58,16 @@ CREATE TABLE `member` (
   `jenis_kelamin` enum('L','P') DEFAULT NULL,
   `telp_member` varchar(15) DEFAULT NULL,
   `no_ktp` varchar(30) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `member`
 --
 
 INSERT INTO `member` (`id_member`, `nama_member`, `alamat_member`, `jenis_kelamin`, `telp_member`, `no_ktp`) VALUES
-(5, 'Abang Fikri', 'Ciamis', 'L', '0821340411', '123456789');
+(10, 'Hadi Akram Ramadhan', 'Pinang Ranti', 'L', '123', '31751234567890'),
+(11, 'Akram', 'Jakarta', 'L', '1234534', '31750987654321'),
+(12, 'Hadi', 'Taman Mini', 'L', '12982932', '31750987667890');
 
 -- --------------------------------------------------------
 
@@ -78,14 +80,16 @@ CREATE TABLE `outlet` (
   `nama_outlet` varchar(100) DEFAULT NULL,
   `alamat_outlet` text DEFAULT NULL,
   `telp_outlet` varchar(15) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `outlet`
 --
 
 INSERT INTO `outlet` (`id_outlet`, `nama_outlet`, `alamat_outlet`, `telp_outlet`) VALUES
-(26, 'Outlet Mawar', 'Jalan Mawar', '081243555656');
+(29, 'Outlet Pinang Ranti', 'Pinang Ranti', '0813'),
+(30, 'Outlet Pasar Minggu', 'Pasar Minggu', '0813'),
+(31, 'Outlet Komplek Kopti', 'Komplek Kopti', '0813');
 
 -- --------------------------------------------------------
 
@@ -99,14 +103,16 @@ CREATE TABLE `paket` (
   `nama_paket` varchar(100) DEFAULT NULL,
   `harga` int(11) DEFAULT NULL,
   `outlet_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `paket`
 --
 
 INSERT INTO `paket` (`id_paket`, `jenis_paket`, `nama_paket`, `harga`, `outlet_id`) VALUES
-(4, 'kiloan', 'Paket Kering Wangi', 15000, 26);
+(6, 'kiloan', 'Paket Murah Meriah', 12000, 29),
+(7, 'bedcover', 'Paket Raja Arab', 50000, 29),
+(8, 'kaos', 'Paket Elon Musk', 90000, 31);
 
 -- --------------------------------------------------------
 
@@ -128,14 +134,15 @@ CREATE TABLE `transaksi` (
   `status` enum('baru','proses','selesai','diambil') DEFAULT NULL,
   `status_bayar` enum('dibayar','belum') DEFAULT NULL,
   `user_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `transaksi`
 --
 
 INSERT INTO `transaksi` (`id_transaksi`, `outlet_id`, `kode_invoice`, `member_id`, `tgl`, `batas_waktu`, `tgl_pembayaran`, `biaya_tambahan`, `diskon`, `pajak`, `status`, `status_bayar`, `user_id`) VALUES
-(33, 26, 'DRY202003213927', 5, '2020-03-21 02:27:48', '2020-03-28 12:00:00', '2020-03-21 02:28:03', 0, 0, 0, 'diambil', 'dibayar', 11);
+(38, 31, 'DRY202502260939', 10, '2025-02-26 01:39:38', '2025-03-05 12:00:00', '2025-02-26 01:41:05', 20000, 1000, 2000, 'selesai', 'dibayar', 1),
+(39, 29, 'DRY202502264320', 10, '2025-02-26 02:21:05', '2025-03-05 12:00:00', '2025-02-26 02:23:25', 1000, 90000, 1000, 'baru', 'dibayar', 15);
 
 -- --------------------------------------------------------
 
@@ -149,17 +156,18 @@ CREATE TABLE `user` (
   `username` varchar(32) DEFAULT NULL,
   `password` varchar(150) DEFAULT NULL,
   `outlet_id` int(11) DEFAULT NULL,
-  `role` enum('admin','kasir','owner') DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `role` enum('admin','kasir','owner') DEFAULT NULL,
+  `img` longblob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id_user`, `nama_user`, `username`, `password`, `outlet_id`, `role`) VALUES
-(1, 'Admin', 'admin', '21232f297a57a5a743894a0e4a801fc3', NULL, 'admin'),
-(10, 'Bapak Mawar', 'mawar', 'bd117502364227fd8c09098d31e11313', 26, 'owner'),
-(11, 'kasirmawar', 'kasirmawar', 'b1890f61964dde27871070f0853cc72f', 26, 'kasir');
+INSERT INTO `user` (`id_user`, `nama_user`, `username`, `password`, `outlet_id`, `role`, `img`) VALUES
+(14, 'Hadi Akram Ramadhan', 'admin', '21232f297a57a5a743894a0e4a801fc3', NULL, 'admin', ''),
+(15, 'Hadi (Kasir)', 'kasir', 'c7911af3adbd12a035b289556d96470a', 29, 'kasir', ''),
+(18, 'Akram (Owner)', 'owner', '72122ce96bfec66e2396d2e25225d70a', 29, 'owner', '');
 
 --
 -- Indexes for dumped tables
@@ -216,37 +224,37 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `detail_transaksi`
 --
 ALTER TABLE `detail_transaksi`
-  MODIFY `id_detail` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id_detail` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `member`
 --
 ALTER TABLE `member`
-  MODIFY `id_member` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_member` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `outlet`
 --
 ALTER TABLE `outlet`
-  MODIFY `id_outlet` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `id_outlet` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- AUTO_INCREMENT for table `paket`
 --
 ALTER TABLE `paket`
-  MODIFY `id_paket` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_paket` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `transaksi`
 --
 ALTER TABLE `transaksi`
-  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- Constraints for dumped tables
